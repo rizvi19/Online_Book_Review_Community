7ebdb03c856dc93bbf76b118f559f5a6182e0bf9
Shahriar Rizvi <44527871+rizvi19@users.noreply.github.com>
2024-09-23 20:32:21 +0600

    Update urls.py for LogOut

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
-    path("logout/", auth_views.LogoutView.as_view(next_page='/'), name="logout"),
+    # path("logout/", auth_views.LogoutView.as_view(next_page='/'), name="logout"),
     path("signup/", views.SignUp.as_view(), name="signup"),
+    path('logout/', views.logout_view, name='logout'),
 ]
